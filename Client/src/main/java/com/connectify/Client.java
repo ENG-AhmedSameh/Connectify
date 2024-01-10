21fd21204f5e281315c6e9785c67ed82d3889b93
Ahmed Sameh <eng.ahmedsameh99@gmail.com>
2024-01-10 21:16:25 +0200

    Split panes

--- Client/src/main/java/com/connectify/Client.java
+++ Client/src/main/java/com/connectify/Client.java
@@ -2,9 +2,10 @@ package com.connectify;
 
 import com.connectify.loaders.ViewLoader;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -13,15 +14,16 @@ import java.io.IOException;
 public class Client extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-//        ViewLoader loader = new ViewLoader();
-//        Parent root = loader.getMainPane();
-//        Scene scene = new Scene(root);
-//        stage.initStyle(StageStyle.UNDECORATED);
-//        stage.setScene(scene);
-//        stage.show();
-        FXMLLoader fxmlLoader = new FXMLLoader(Client.class.getResource("/views/SignUpPane.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        ViewLoader loader = ViewLoader.getInstance();
+        BorderPane mainPane = loader.getMainBorderPane();
+        GridPane centerPane =(GridPane)mainPane.getCenter();
+        centerPane.add(loader.getLogoAnchorPane(),0,0);
+        centerPane.add(loader.getSignUpAnchorPane(),1,0);
+        //mainPane.setTop(loader.getTitleBarHBox());
+        mainPane.setCenter(centerPane);
+        Parent root = mainPane;
+        Scene scene = new Scene(root);
+        //stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
         stage.setMinWidth(750);
--- Client/src/main/java/com/connectify/loaders/ViewLoader.java
+++ Client/src/main/java/com/connectify/loaders/ViewLoader.java
@@ -1,18 +1,88 @@
 package com.connectify.loaders;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+
 import java.io.IOException;
 public class ViewLoader {
-    public Pane getMainPane() {
+
+
+    BorderPane mainBorderPane;
+    AnchorPane logoAnchorPane;
+    AnchorPane signUpAnchorPane;
+
+    HBox titleBarHBox;
+
+    private ViewLoader(){
+        loadMainPane();
+        //loadTitleBar();
+        loadLogoPane();
+        loadSignUpPane();
+    }
+
+    private void loadMainPane() {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/views/MainPane.fxml"));
+        try {
+            mainBorderPane =fxmlLoader.load();
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void loadTitleBar(){
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/views/titleBarPane.fxml"));
+        try {
+            mainBorderPane =fxmlLoader.load();
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void loadSignUpPane(){
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("/views/SignUpPane.fxml"));
-        Pane mainPane = null;
         try {
-            mainPane = fxmlLoader.load();
+            signUpAnchorPane = fxmlLoader.load();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return mainPane;
+    }
+
+    private void loadLogoPane(){
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/views/logoPane.fxml"));
+        try {
+            logoAnchorPane = fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    private static final ViewLoader viewLoader = new ViewLoader();
+    public static ViewLoader getInstance() {
+        return viewLoader;
+    }
+
+    public AnchorPane getSignUpAnchorPane() {
+        return signUpAnchorPane;
+    }
+
+
+    public BorderPane getMainBorderPane() {
+        return mainBorderPane;
+    }
+
+    public AnchorPane getLogoAnchorPane() {
+        return logoAnchorPane;
+    }
+
+    public HBox getTitleBarHBox() {
+        return titleBarHBox;
     }
 }
