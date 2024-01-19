6197ebd626d8cc6b3a8a8c6e85016c3e81f7dbee
Mohamed Nofal <nofal.a.mohamed@gmail.com>
2024-01-19 21:20:04 +0200

    add ContactsDAOImpl implementation

---
+++ Server/src/main/java/com/connectify/model/dao/impl/ContactsDAOImpl.java
@@ -0,0 +1,87 @@
+package com.connectify.model.dao.impl;
+
+import com.connectify.model.dao.ContactsDAO;
+import com.connectify.model.entities.Contacts;
+import com.connectify.controller.utils.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ContactsDAOImpl implements ContactsDAO {
+
+    private final DBConnection dbConnection;
+
+    public ContactsDAOImpl() {
+        dbConnection = DBConnection.getInstance();
+    }
+
+    @Override
+    public boolean insert(Contacts contacts) {
+        String query = "INSERT INTO contacts (user, contact) VALUES (?, ?)";
+        try (Connection connection = dbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, contacts.getUser());
+            preparedStatement.setString(2, contacts.getContact());
+
+            int rowsInserted = preparedStatement.executeUpdate();
+            return rowsInserted > 0;
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public Contacts get(String key) {
+        String query = "SELECT * FROM contacts WHERE user = ?";
+        try (Connection connection = dbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, key);
+            Contacts contacts = null;
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    contacts = new Contacts();
+                    contacts.setUser(resultSet.getString("user"));
+                    contacts.setContact(resultSet.getString("contact"));
+                }
+            }
+            return contacts;
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public boolean update(Contacts contacts) {
+        String query = "UPDATE contacts SET contact = ? WHERE user = ?";
+        try (Connection connection = dbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, contacts.getContact());
+            preparedStatement.setString(2, contacts.getUser());
+
+            int rowsUpdated = preparedStatement.executeUpdate();
+            return rowsUpdated > 0;
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public boolean delete(String key) {
+        String query = "DELETE FROM contacts WHERE user = ?";
+        try (Connection connection = dbConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, key);
+            int rowsDeleted = preparedStatement.executeUpdate();
+            return rowsDeleted > 0;
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+            return false;
+        }
+    }
+
+}
