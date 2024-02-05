2dc8a41d4b06d48f53b6b330b80d7a653f8c05cc
hassanarafa <hassan.arafa.e@gmail.com>
2024-02-05 14:57:13 +0200

    add imageConverterClass

---
+++ Client/src/main/java/com/connectify/utils/ImageConverter.java
@@ -0,0 +1,24 @@
+package com.connectify.utils;
+
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+
+import java.io.ByteArrayInputStream;
+
+public class ImageConverter {
+
+    public static ImagePattern convertBytesToImagePattern(byte[] imageBytes) {
+        Image image;
+        if (imageBytes == null || imageBytes.length == 0) {
+            image = new Image(ImageConverter.class.getResourceAsStream("/Images/profile.png"));
+            return new ImagePattern(image);
+        }
+        try {
+            image = new Image(new ByteArrayInputStream(imageBytes));
+            return new ImagePattern(image);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
