f74729ddc5bb305a7abbdfd313939cb7b6604fd3
Mohamed Nofal <nofal.a.mohamed@gmail.com>
2024-02-06 17:32:49 +0200

    remove unnecessary code

--- Server/src/main/java/com/connectify/services/GroupService.java
+++ Server/src/main/java/com/connectify/services/GroupService.java
@@ -13,27 +13,6 @@ import java.util.List;
 
 public class GroupService {
 
-    public boolean areAlreadyFriends(String userPhone, String friendPhone) {
-        ContactsDAO contactsDAO = new ContactsDAOImpl();
-        return contactsDAO.areAlreadyFriends(userPhone, friendPhone);
-    }
-
-    public void notifyContacts(String phoneNumber, String title, String body){
-        ContactsDAO contactsDAO = new ContactsDAOImpl();
-        var contacts = contactsDAO.getContactsList(phoneNumber);
-        var connectedUsers = Server.getConnectedUsers();
-        for(var contact : contacts){
-            var connectedContact = connectedUsers.get(contact.getPhoneNumber());
-            if(connectedContact != null){
-                try {
-                    connectedContact.receiveNotification(title, body);
-                } catch (RemoteException e) {
-                    System.err.println("Remote Exception: " + e.getMessage());
-                }
-            }
-        }
-    }
-
     public boolean createGroup(List<ContactsDTO> contactsDTOS, String groupName, String groupDescription, byte[] image) {
         GroupDAO groupDAO = new GroupDAOImpl();
         return groupDAO.createGroup(contactsDTOS, groupName, groupDescription, image);
